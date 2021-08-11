ff2df7be805de2c6ab3357d9ed92d5b71e85ec22
sauravjaiswa <sauravjaiswal520@gmail.com>
2021-08-11 15:12:19 +0530

    Added Inventory

---
+++ Inventory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vehicle_Inventory
+{
+    public class Inventory
+    {
+        private readonly IList<IVehicle> _vehicles;
+
+        public Inventory()
+        {
+            _vehicles = new List<IVehicle>();
+        }
+
+        private int GetAge(int year)
+        {
+            return (DateTime.Now.Year - year);
+        }
+
+        private void Display(IVehicle vehicle)
+        {
+            Console.WriteLine($"Owner Name - {vehicle.Owner.Name}" +
+                    $"\nAddress - {vehicle.Owner.Address}" +
+                    $"\nModel - {vehicle.Model}" +
+                    $"\nYear - {vehicle.Year}" +
+                    $"\nPurchase Value - Rs {vehicle.PurchaseValue}" +
+                    $"\nDistance - {vehicle.Distance} km" +
+                    $"\nSale Price - Rs {vehicle.SalePrice()}");
+        }
+
+        public void Add(IVehicle vehicle)
+        {
+            if (GetAge(vehicle.Year) > 10)
+            {
+                return;
+            }
+            _vehicles.Add(vehicle);
+        }
+
+        public void DisplayAll()
+        {
+            Console.WriteLine("All Vehicles ->");
+            foreach (IVehicle vehicle in _vehicles)
+            {
+                Display(vehicle);
+                Console.WriteLine("------------------------------------------");
+            }
+        }
+
+        public void AgeSearch(int years)
+        {
+            var query = _vehicles.Where(v => (GetAge(v.Year) == years));
+            
+            Console.WriteLine($"All Vehicles of {years} years->");
+            foreach (IVehicle vehicle in query)
+            {
+                Display(vehicle);
+                Console.WriteLine("------------------------------------------");
+            }
+        }
+
+        public void SalesPriceSearch(double price)
+        {
+            var query = _vehicles.Where(v => (v.SalePrice() == price));
+
+            Console.WriteLine($"All Vehicles of Rs {price}->");
+            foreach (IVehicle vehicle in query)
+            {
+                Display(vehicle);
+                Console.WriteLine("------------------------------------------");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,51 @@ namespace Vehicle_Inventory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Inventory inventory = new Inventory();
+            string ch;
+            int age;
+            double sp;
+
+            do
+            {
+                Console.WriteLine("1) Adding a Vehicle");
+                Console.WriteLine("2) Displaying all vehicles");
+                Console.WriteLine("3) Search vehicle based on Age");
+                Console.WriteLine("4) Search vehicle based on Sale Price");
+                Console.WriteLine("5) Exit");
+                Console.WriteLine("Enter a choice:");
+                ch = Console.ReadLine();
+
+                switch (ch)
+                {
+                    case "1":
+                        inventory.Add(new Car() { Owner = new Owner { Name = "A", Address = "XYZ" }, Model = "SUV908", Year = 2015, PurchaseValue = 5264921, Distance = 10000 });
+                        inventory.Add(new Bike() { Owner = new Owner { Name = "B", Address = "HDJS" }, Model = "XYW765", Year = 2012, PurchaseValue = 45649, Distance = 5000 });
+                        inventory.Add(new Bike() { Owner = new Owner { Name = "C", Address = "DHS" }, Model = "HB165", Year = 2005, PurchaseValue = 5264921, Distance = 19000 });
+                        inventory.Add(new Car() { Owner = new Owner { Name = "D", Address = "CCU" }, Model = "UYR657", Year = 2013, PurchaseValue = 5264921, Distance = 51000 });
+                        break;
+                    case "2":
+                        inventory.DisplayAll();
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter age to be searched for : ");
+                        age = int.Parse(Console.ReadLine());
+                        inventory.AgeSearch(age);
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter sales price to be searched for : ");
+                        sp = double.Parse(Console.ReadLine());
+                        inventory.SalesPriceSearch(sp);
+                        break;
+                    case "5":
+                        Console.WriteLine("Exiting...");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input! Try again...");
+                        break;
+                }
+            } while (true);
         }
     }
 }
